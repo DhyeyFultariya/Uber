03d0695d09ab16b0f4686261bae242759fd7b548
Dhyey <dhyeypf@gmail.com>
2025-01-02 20:22:39 +0530

    rafce

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Router} from 'react-router-dom'
+
 
 const App = () => {
   return (
     <div>
-      
+      <Router path="/" element={ <Home /> } />
     </div>
   )
 }
 
-export default App
+export default App 
