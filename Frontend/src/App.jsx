9819d80c191f54c856c705aa27165dccd5554947
Dhyey <dhyeypf@gmail.com>
2025-01-03 23:06:25 +0530

    make basic routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import UserLogin from './Pages/UserLogin.jsx'
+import UserSignup from './Pages/UserSignup'
+import CaptainSignup from './Pages/CaptainSignup.jsx'
+import CaptainLogin from './Pages/CaptainLogin.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/login" element={ <UserLogin /> } />
+        <Route path="/singup" element={ <UserSignup /> } />
+        <Route path="/captain-signup" element={ <CaptainSignup /> } />
+        <Route path="/captain-login" element={ <CaptainLogin /> } />
+
+      </Routes>
+    </div>
   )
 }
 
