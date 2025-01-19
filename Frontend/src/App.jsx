b48136113fa1023ad4525476e59d3bd0a14417d5
Dhyey <dhyeypf@gmail.com>
2025-01-19 12:51:32 +0530

    integrate users routes login logout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home.jsx'
+import Start from './Pages/Start.jsx'
 import UserLogin from './Pages/UserLogin.jsx'
 import UserSignup from './Pages/UserSignup'
 import CaptainSignup from './Pages/CaptainSignup.jsx'
 import CaptainLogin from './Pages/CaptainLogin.jsx'
+import Home from './Pages/Home.jsx'
+import UserProtectWrapper from './Pages/UserProtectWrapper.jsx'
+import UserLogout from './Pages/UserLogout.jsx'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={ <Home /> } />
+        <Route path="/" element={ <Start /> } />
+        <Route path="/home" element={ <UserProtectWrapper> <Home /> </UserProtectWrapper>} />
         <Route path="/login" element={ <UserLogin /> } />
         <Route path="/singup" element={ <UserSignup /> } />
         <Route path="/captain-signup" element={ <CaptainSignup /> } />
         <Route path="/captain-login" element={ <CaptainLogin /> } />
+
+        <Route path='/user/logout' element={ <UserProtectWrapper> <UserLogout /> </UserProtectWrapper>  } />
 
       </Routes>
     </div>
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
-      <div className='bg-cover bg-[url("Ride-with-Uber.webp")] h-screen pt-8 w-full flex justify-between flex-col'>
-        <img src="/uber-logo.png" alt="" className='w-28 ml-8'/>
-        <div className='bg-white pb-7 py-4 px-4'>
-          <h2 className='text-3xl font-bold'>Get Start with Uber</h2>
-          <Link to={'/login'} className='flex items-center justify-center w-full bg-black text-white py-3 mt-8 rounded'>Continue</Link>
-        </div>
-      </div>
-    </div>
+    <div>Home</div>
   )
 }
 
